0cd02af84c0f8598f8f79d82c307e3dbc4af1108
Sunil Tailor <sunil.tailor@indexfeed.com>
2018-05-24 23:35:57 +0100

    updated to store state in Git repository

--- bin/createNewReqBranch.sh
+++ bin/createNewReqBranch.sh
@@ -1,8 +1,13 @@
 #!/bin/bash 
 
-
-
 REQ_BRANCH=$(./bin/stateNextReq.sh)
+
+echo $REQ_BRANCH >> ./state/requests.txt
+git add ./state/requests.txt
+git commit -am "updated requests.txt with new REQ  ${REQ_BRANCH}"
+git push origin master
+
+# Timestamp for SQL script
 TS=$(echo $REQ_BRANCH | cut -d'-' -f 3)
 
 git checkout -b "REQ-${REQ_BRANCH}"
@@ -26,3 +31,8 @@ cd updates/${REQ_BRANCH}/BACKOUT_SCRIPTS/
 echo '# Sample SQL file filename is the order in which scripts are executed' > 001-$TS.sql
 echo '# Sample SQL file filename is the order in which scripts are executed' > 002-$TS.sql
 echo ${REQ_BRANCH}
+
+git add updates/${REQ_BRANCH}/*
+get commit 'First Commit for Branch REQ-${REQ_BRANCH}'
+get push origin "REQ-${REQ_BRANCH}"
+ 
